d9900401a93bdc35f1ec4098531f1983bd991931
Emil Świderek <emil.swiderek@toxic-software.pl>
2015-05-12 19:17:21 +0200

    Graph

--- generator/main.py
+++ generator/main.py
@@ -3,6 +3,10 @@ from generator.heart_gen import HeartGenerator
 from responseFunction.forwardingFunction import ForwardingFunction
 from responseFunction.sinusFunction import SinusFunction
 from analysis.phaseDifferences import PhaseDifferences
+from responseFunction.Akselrod import Akselrod
+import matplotlib.pyplot as plt
+import numpy as np
+import generator.helper as hp
 
 
 def main():
@@ -14,26 +18,36 @@ def main():
     HeartGen.setResponseFunction(getResponseFunction('sinus'))
     heartFunction = HeartGen.generateProcess()
 
-    breath_and_heart_printer(breathFunction, heartFunction)
+    bind = bind_breath_and_heart(breathFunction, heartFunction)
     phaseAnalyzer = PhaseDifferences()
     indexes, results = phaseAnalyzer.analyze(breathFunction, heartFunction)
     print("Results: ")
     print(results)
     print(indexes)
 
+    plt.plot(bind[0], bind[1])
+    plt.plot(bind[0], bind[2])
+    plt.show()
+
+
 
 def getResponseFunction(name):
     # @todo set as parameter name of response function
     return {
         'forwarding': ForwardingFunction(),
-        'sinus': SinusFunction()
+        'sinus': SinusFunction(),
+        'akselrod': Akselrod(),
     }[name]
 
-def breath_and_heart_printer(breathFunction, heartFunction):
+def bind_breath_and_heart(breathFunction, heartFunction):
     print("Iteration:".ljust(10),"Breath phase:".ljust(20), "Heart phase:".ljust(20))
+    bind = np.ndarray(shape=(3, len(breathFunction)))
     for x in range(0, len(breathFunction)):
         print(str(x).ljust(10), str(breathFunction[x]).ljust(20), str(heartFunction[x]).ljust(20))
-
+        bind[0][x] = x/hp.breath_period
+        bind[1][x] = breathFunction[x]/hp.breath_period
+        bind[2][x] = heartFunction[x]/hp.heart_period
+    return bind
 
 if __name__ == "__main__":
     main()
